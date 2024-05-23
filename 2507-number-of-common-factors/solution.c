4dfcfdd210e4d290c0805a05f6e9516d8ac8840c
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-05-23 04:21:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 5.2 MB (100.00%)

---
+++ 2507-number-of-common-factors/solution.c
@@ -0,0 +1,10 @@
+int commonFactors(int a, int b) {
+    int count=0;
+    for(int i=1;i<=(a<b?b:a);i++){
+        if(a%i==0 && b%i==0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
