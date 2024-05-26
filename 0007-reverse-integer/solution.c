35e6046010192b16dd7105475fd8bb57109d6279
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-05-26 09:10:45 +0000

    [LeetCode Sync] Runtime - 5 ms (23.19%), Memory - 5.3 MB (100.00%)

---
+++ 0007-reverse-integer/solution.c
@@ -0,0 +1,12 @@
+int reverse(int x){
+    long rev=0;
+    while(x!=0){
+        int rem=x%10;
+        int sum=rev*10+rem;
+        if((sum-rem)/10!=rev)
+            return 0;
+        rev=sum;
+        x/=10;
+    }
+    return rev;
+}
