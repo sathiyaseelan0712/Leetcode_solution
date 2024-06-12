219b3aedcabe9933c5c696717753b3be8305db65
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-06-12 05:58:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.3 MB (98.77%)

--- 0026-remove-duplicates-from-sorted-array/solution.java
+++ 0026-remove-duplicates-from-sorted-array/solution.java
@@ -3,8 +3,7 @@ class Solution {
     int count=0;
     for(int i=1;i<nums.length;i++){
         if(nums[i]!=nums[count]){
-            count++;
-            nums[count]=nums[i];
+            nums[++count]=nums[i];
         }
 
     }
