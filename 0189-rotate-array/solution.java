fa8cec975a864b20aaede1aba2e67cecd95d37a9
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-06-12 06:17:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 57.3 MB (70.87%)

--- 0189-rotate-array/solution.java
+++ 0189-rotate-array/solution.java
@@ -1,19 +1,15 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int le = nums.length;
-        int[] arr = new int[le];
-        for(int i = 0; i < le; i++) {
-            arr[(i + k) % le] = nums[i];
-        }
-        for(int i = 0; i < le; i++) {
-            nums[i] = arr[i];
-        }
-        for(int i = 0; i < le; i++) {
-            System.out.print(nums[i]);
-            if (i != le - 1) {
-                System.out.print(", ");
-            }
+        int n = nums.length;
+        k = k%n;
+        swap(nums,0,n-1);
+        swap(nums,0,k-1);
+        swap(nums,k,n-1);
+    }
+	public void swap(int[] nums, int start, int end){
+        while(start<end){
+        	nums[start] = nums[end]+nums[start]-(nums[end]=nums[start]);
+            start++;end--;
         }
     }
 }
-
