2c7ad09a4be4b33c38b7bcf2adb547cd6d90460d
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-08-19 10:35:37 +0000

    [LeetCode Sync] Runtime - 35 ms (11.73%), Memory - 58.2 MB (61.92%)

--- 0287-find-the-duplicate-number/solution.java
+++ 0287-find-the-duplicate-number/solution.java
@@ -1,9 +1,9 @@
 class Solution {
     public int findDuplicate(int[] nums) {
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==nums[i+1])
-            {
+        int n=nums.length;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
                 return nums[i];
             }
         }
