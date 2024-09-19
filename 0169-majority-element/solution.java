d8498023286cbf9e6616e83c6e8fa0bdbfa62df5
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-09-19 17:08:40 +0000

    [LeetCode Sync] Runtime - 4 ms (52.21%), Memory - 53 MB (59.17%)

--- 0169-majority-element/solution.java
+++ 0169-majority-element/solution.java
@@ -1,19 +1,8 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int a=nums[0];
-        int c=1;
-        for(int i=1;i<nums.length;i++){
-            if(c==0){
-                a=nums[i];
-                c=1;
-            }
-            else if(nums[i]==a){
-                c++;
-            }
-            else{
-                c--;
-            }
-        }
-        return a;
+       Arrays.sort(nums);
+       int n=nums.length;
+       int a=n/2;
+       return nums[a];
     }
 }
