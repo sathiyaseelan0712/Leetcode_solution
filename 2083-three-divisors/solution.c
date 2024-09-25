d30bf29f7b595bbc51947ec18f5b66d2138b5e3b
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-09-25 15:34:05 +0000

    [LeetCode Sync] Runtime - 4 ms (0.28%), Memory - 7.4 MB (96.94%)

---
+++ 2083-three-divisors/solution.c
@@ -0,0 +1,12 @@
+bool isThree(int n) {
+    int count=1;
+    for(int i=2;i<=n;i++){
+        if(n%i==0){
+            count+=1;
+        }
+    }
+    if(count==3){
+        return true;
+    }
+    return 0;
+}
