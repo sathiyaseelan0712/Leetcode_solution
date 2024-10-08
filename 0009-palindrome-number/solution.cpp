7ddc6b48a353d54e873ca6eac8efd2cbd9d231ee
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-10-08 15:44:02 +0000

    [LeetCode Sync] Runtime - 12 ms (6.07%), Memory - 8.5 MB (56.89%)

---
+++ 0009-palindrome-number/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long rev=0,rem=0,n=x;
+        while(n>0){
+            rem=n%10;
+            rev=(rev*10)+rem;
+            n/=10;
+        }
+        if(x==rev){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+};
