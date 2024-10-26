be2474b9585a4fa85c7bcea9c6ece0d2e46f1d9a
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-10-26 17:39:29 +0000

    [LeetCode Sync] Runtime - 238 ms (68.65%), Memory - 0B (100.00%)

---
+++ 1801-average-time-of-process-per-machine/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp),3)
+AS processing_time
+FROM Activity as a1 join Activity a2 
+on a1.machine_id=a2.machine_id and a1.process_id = a2.process_id
+and a1.activity_type='start' and a2.activity_type='end'
+group by a1.machine_id;
+
