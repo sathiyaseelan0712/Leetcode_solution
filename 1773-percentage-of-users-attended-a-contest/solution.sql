86cfc8068685d303d7d52bf0c511eda879f49fdb
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-10-27 14:24:27 +0000

    [LeetCode Sync] Runtime - 2402 ms (8.72%), Memory - 0B (100.00%)

---
+++ 1773-percentage-of-users-attended-a-contest/solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT r.contest_id , ROUND(COUNT(DISTINCT r.user_id)/ COUNT(DISTINCT u.user_id)*100,2)
+AS percentage
+FROM Users u
+JOIN Register as r
+GROUP BY r.contest_id
+ORDER BY 2 DESC, 1 ASC
