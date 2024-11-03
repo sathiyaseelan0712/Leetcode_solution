479414f4abebde77800b92509b7acdaa99ef8f4c
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-11-03 17:16:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.4 MB (51.75%)

--- 0812-rotate-string/solution.java
+++ 0812-rotate-string/solution.java
@@ -1,12 +1,8 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length())
-        return false;
-        String a=s+s;
-        int index=a.indexOf(goal);
-        if(index>=0)
-        return true;
-        else
-        return false;
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        return (s + s).contains(goal);
     }
 }
