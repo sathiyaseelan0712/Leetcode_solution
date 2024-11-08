0acd6f438adedfbb1e08043e9b70441669efdfc7
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-11-08 06:27:21 +0000

    [LeetCode Sync] Runtime - 480 ms (73.19%), Memory - 0B (100.00%)

--- 0197-rising-temperature/solution.sql
+++ 0197-rising-temperature/solution.sql
@@ -1,1 +1,3 @@
-SELECT w2.id from weather w1, weather w2 where datediff(w2.recordDate , w1.recordDate) = 1 AND w2.temperature > w1.temperature;
+SELECT w2.id 
+FROM weather w1, weather w2
+WHERE datediff(w2.recordDate , w1.recordDate) = 1 AND w2.temperature > w1.temperature;
