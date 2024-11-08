bb13d656fa44535f5a2066d1b800a21c25bb88dd
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-11-08 08:44:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.6 MB (86.76%)

---
+++ 0042-trapping-rain-water/solution.c
@@ -0,0 +1,21 @@
+int trap(int* h, int n) {
+    int left = 0, right = n - 1, lm = 0, rm = 0, res = 0;
+    while (left < right) {
+        if (h[left] < h[right]) {
+            if (lm < h[left]) {
+                lm = h[left];
+            } else {
+                res += lm - h[left];
+            }
+            ++left;
+        } else {
+            if (rm < h[right]) {
+                rm = h[right];
+            } else {
+                res += rm - h[right];
+            }
+            --right;
+        }
+    }
+    return res;
+}
