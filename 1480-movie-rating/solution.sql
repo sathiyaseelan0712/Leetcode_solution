efa3aab76401c6a6441f393bb9c0ded4232f7725
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-12-18 13:45:20 +0000

    [LeetCode Sync] Runtime - 1133 ms (92.05%), Memory - 0B (100.00%)

---
+++ 1480-movie-rating/solution.sql
@@ -0,0 +1,12 @@
+(SELECT name as results
+FROM MovieRating JOIN Users USING(user_id)
+GROUP BY name
+ORDER BY COUNT(*) DESC,name LIMIT 1)
+
+UNION ALL
+
+(SELECT title as results
+FROM MovieRating JOIN Movies USING(movie_id)
+WHERE EXTRACT(YEAR_MONTH FROM created_at)=202002
+GROUP BY title 
+ORDER BY AVG(rating) DESC, title Limit  1);
