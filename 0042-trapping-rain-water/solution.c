810dbb96927c3373e16bffac18934ebaee204f84
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-12-27 13:06:03 +0000

    [LeetCode Sync] Runtime - 7 ms (3.06%), Memory - 9.7 MB (59.85%)

--- 0042-trapping-rain-water/solution.c
+++ 0042-trapping-rain-water/solution.c
@@ -1,21 +1,26 @@
 int trap(int* h, int n) {
-    int left = 0, right = n - 1, lm = 0, rm = 0, res = 0;
-    while (left < right) {
-        if (h[left] < h[right]) {
-            if (lm < h[left]) {
-                lm = h[left];
-            } else {
-                res += lm - h[left];
-            }
-            ++left;
-        } else {
-            if (rm < h[right]) {
-                rm = h[right];
-            } else {
-                res += rm - h[right];
-            }
-            --right;
-        }
+    int left[n];
+    int right[n];
+    int ans[n];
+    int max=h[0],max1=h[n-1];
+    for(int i=0;i<n;i++){
+        max = fmax(h[i],max);
+        left[i] = max;
     }
-    return res;
+    for(int i=n-1;i>=0;i--){
+        max1 = fmax(h[i],max1);
+        right[i]=max1;
+    }
+    int sum=0;
+    for(int i=0;i<n;i++){
+        max = fmin(left[i],right[i]);
+        ans[i]=max;
+        sum+=ans[i];
+    }
+    int sum1=0;
+    for(int i=0;i<n;i++){
+        sum1+=h[i];
+    }
+    printf("%d %d",sum,sum1);
+    return sum-sum1;
 }
