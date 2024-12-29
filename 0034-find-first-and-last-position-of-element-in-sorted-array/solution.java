7a5b7cfe3b29cd71560210778b7838f1648443cb
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-12-29 08:29:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 46.2 MB (22.87%)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -1,22 +1,45 @@
 class Solution {
-  public int[] searchRange(int[] nums, int target) {
-    final int l = firstGreaterEqual(nums, target);
-    if (l == nums.length || nums[l] != target)
-      return new int[] {-1, -1};
-    final int r = firstGreaterEqual(nums, target + 1) - 1;
-    return new int[] {l, r};
-  }
-
-  private int firstGreaterEqual(int[] A, int target) {
-    int l = 0;
-    int r = A.length;
-    while (l < r) {
-      final int m = (l + r) / 2;
-      if (A[m] >= target)
-        r = m;
-      else
-        l = m + 1;
+    private int startValue(int[] nums, int a) {
+        int n = nums.length;
+        int left = 0, right = n - 1;
+        int val = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == a) {
+                val = mid;
+                right = mid - 1;
+            } else if (nums[mid] > a) {
+                right = mid - 1;
+            } else
+                left = mid + 1;
+        }
+        return val;
     }
-    return l;
-  }
+
+    private int endValue(int[] nums, int a) {
+        int n = nums.length;
+        int left = 0, right = n - 1;
+        int val = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == a) {
+                val = mid;
+                // right = mid-1;
+                left = mid + 1;
+            } else if (nums[mid] > a) {
+                right = mid - 1;
+            } else
+                left = mid + 1;
+        }
+        return val;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int startIdx = startValue(nums, target);
+        int endIdx = endValue(nums, target);
+        int[] ans = new int[2];
+        ans[0] = startIdx;
+        ans[1] = endIdx;
+        return ans;
+    }
 }
