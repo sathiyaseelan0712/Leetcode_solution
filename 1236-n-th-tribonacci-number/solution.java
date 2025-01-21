b13122ec15dd24d0fef21e8d527113574bbf68fa
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-01-21 09:57:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41 MB (9.64%)

---
+++ 1236-n-th-tribonacci-number/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solve(int n,Map<Integer,Integer> dp){
+        if(n == 0 || n == 1) return n;
+        if(n == 2) return 1;
+        if(dp.containsKey(n)) return dp.get(n);
+        int value = solve(n-1,dp)+solve(n-2,dp)+solve(n-3,dp);
+        dp.put(n,value);
+        return value;
+    }
+    public int tribonacci(int n) {
+        Map<Integer,Integer> dp = new HashMap<>();
+        return solve(n,dp);
+    }
+}
