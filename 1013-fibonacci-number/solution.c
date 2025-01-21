bae2b77907bb7aabd678cba8bba078434776411d
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-01-21 06:11:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.7 MB (85.17%)

--- 1013-fibonacci-number/solution.c
+++ 1013-fibonacci-number/solution.c
@@ -1,4 +1,10 @@
-int fib(int n){
+int solve(int n,int* dp){
     if(n==0||n==1) return n;
-    else return fib(n-1)+fib(n-2); 
+    else if(dp[n] != -1) return dp[n];
+    else return dp[n] = solve(n-1,dp) + solve(n-2,dp);
+}
+int fib(int n){
+    int dp[n+1];
+    memset(dp,-1,sizeof(dp));
+    return solve(n,dp);
 }
