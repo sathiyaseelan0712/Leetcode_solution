b0f172bc0f38cd58ab746fa5b7443e1aa56e9c62
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-01-21 05:24:39 +0000

    [LeetCode Sync] Runtime - 9 ms (16.29%), Memory - 8 MB (10.01%)

--- 1013-fibonacci-number/solution.c
+++ 1013-fibonacci-number/solution.c
@@ -1,22 +1,4 @@
-int bitwise_add(int x, int y) {
-    while (y != 0) {
-        int carry = (x & y) << 1;  
-        x = x ^ y;                 
-        y = carry;                
-    }
-    return x;
-}
-
-int fib(int n) {
-    int a = 0, b = 1, temp;
-
-    if (n == 0) return a; 
-    if (n == 1) return b;  
-    for (int i = 2; i <= n; i++) {
-        temp = bitwise_add(a, b); 
-        a = b;
-        b = temp;
-    }
-
-    return b; 
+int fib(int n){
+    if(n==0||n==1) return n;
+    else return fib(n-1)+fib(n-2); 
 }
