723b95c5f6c7be475520a50cc9a68ba4cfbceef2
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-01-21 09:15:58 +0000

    [LeetCode Sync] Runtime - 20 ms (5.19%), Memory - 45 MB (5.93%)

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/solution.java
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/solution.java
@@ -1,23 +1,52 @@
 class Solution {
+    private String addOnes(String s) {
+        int carry = 1;
+        StringBuilder str = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char a = s.charAt(i);
+            if (a == '1' && carry == 1)
+                str.append('0');
+            else if (a == '0' && carry == 1) {
+                str.append('1');
+                carry = 0;
+            } else
+                str.append(a);
+        }
+        if(carry == 1){
+            str.append('1');
+        }
+        return str.reverse().toString();
+    }
+
+    private String divideByTwo(String s) {
+        return '0' + s.substring(0, s.length() - 1);
+    }
+
+    private boolean checkOne(String s){
+        int n = s.length();
+        for(int i = 0; i < n - 1; i++){
+            if(s.charAt(i) == '1'){
+                return false;
+            }
+        }
+        if(s.charAt(n - 1) == '1') 
+            return true;
+        return false;
+    }
+
     public int numSteps(String s) {
-        int steps = 0;
-        int carry = 0;
-        
-        for (int i = s.length() - 1; i > 0; i--) {
-            int digit = s.charAt(i) - '0';
-            if (digit + carry == 1) {
-                carry = 1;
-                steps += 2;  
+        int count = 0;
+        while (!checkOne(s)) {
+            if (s.charAt(s.length() - 1) == '1') {
+                s = addOnes(s);
+                count++;
             } else {
-             
-                steps += 1;
+                s = divideByTwo(s);
+                count++;
             }
         }
 
-        if (carry == 1) {
-            steps += 1;
-        }
-        
-        return steps;
+        return count;
     }
 }
+
