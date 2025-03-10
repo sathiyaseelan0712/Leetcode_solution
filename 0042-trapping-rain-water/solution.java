8c2f80e9195664fe249390e9dabb6376205c38dd
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-03-10 18:09:44 +0000

    [LeetCode Sync] Runtime - 1 ms (63.13%), Memory - 45.9 MB (88.44%)

--- 0042-trapping-rain-water/solution.java
+++ 0042-trapping-rain-water/solution.java
@@ -3,29 +3,32 @@ class Solution {
         int n = h.length;
         int[] left = new int[n];
         int[] right = new int[n];
-        int[] ans = new int[n];
-        int max=h[0];
-        left[0]=h[0];
+        
+        int max = h[0];
+        left[0] = h[0];
         for(int i=1;i<n;i++){
-            max=Math.max(max,h[i]);
-            left[i]=max;
+            max = Math.max(max,h[i]);
+            left[i] = max;
         }
-        int max1=h[n-1];
-        right[n-1]=max1;
-        for(int i=n-1;i>=0;i--){
-            max1=Math.max(max1,h[i]);
-            right[i]=max1;
+
+        int maxR = h[n-1];
+        right[n-1] = h[n-1];
+        for(int i = n-2;i>=0 ;i--){
+           maxR = Math.max(maxR,h[i]);
+            right[i] = maxR;
         }
-        int min=0;
-        int sum=0;
+
+        int[] ans = new int[n];
+        int sum = 0;
         for(int i=0;i<n;i++){
-            min=Math.min(left[i],right[i]);
-            ans[i]=min;
-            sum+=ans[i];
+            int min = Math.min(left[i],right[i]);
+            ans[i] = min;
+            sum += min;
         }
-        int sum1=0;
-        for(int i=0;i<n;i++){
-            sum1+=h[i];
+
+        int sum1 = 0;
+        for(int i : h){
+            sum1 += i;
         }
         return sum-sum1;
     }
