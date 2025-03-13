cee205eb8160210b303aa410e234e4709f707ecd
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-03-13 19:29:58 +0000

    [LeetCode Sync] Runtime - 44 ms (8.31%), Memory - 41 MB (24.09%)

--- 2571-find-the-pivot-integer/solution.java
+++ 2571-find-the-pivot-integer/solution.java
@@ -1,7 +1,15 @@
 class Solution {
-  public int pivotInteger(int n) {
-    final int y = (n * n + n) / 2;
-    final int x = (int) Math.sqrt(y);
-    return x * x == y ? x : -1;
-  }
+    public int sum(int start,int end){
+        int s =0;
+        for(int i = start;i<=end;i++) s+=i;
+        return s;
+    }
+    public int pivotInteger(int n) {
+        for(int i=1;i<=n;i++){
+            if(sum(1,i) == sum(i,n)){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
