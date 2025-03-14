64f7f66175286a008d5f333bb8bdbf877b992691
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-03-14 06:00:34 +0000

    [LeetCode Sync] Runtime - 2 ms (87.86%), Memory - 55.4 MB (75.32%)

--- 0238-product-of-array-except-self/solution.java
+++ 0238-product-of-array-except-self/solution.java
@@ -1,21 +1,25 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int ans[] = new int[n];
-        ans[0] = 1;
-        for (int i = 1; i < n; i++) {
-            ans[i] = nums[i - 1] * ans[i - 1];
-        }
-        // for(int i=0;i<n;i++){
-            // System.out.println(ans);
-    // }        
-        int right = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            ans[i] = ans[i] * right;
-            right = right * nums[i];
-        }
-            // System.out.println(ans);
+        int[] left = new int[n];
+        int[] right =new int[n];
+        left[0] = 1;
+        right[n-1] = 1;
 
-        return ans;
+        for(int i=1;i<n;i++){
+            left[i] = left[i-1] * nums[i-1]; 
+        }
+
+        for(int i = n-2;i>=0;i--){
+            right[i] = right[i+1] * nums[i+1];
+        }
+
+        for(int i=0;i<n;i++){
+            nums[i] = left[i] * right[i];
+        }
+        
+        return nums;
     }
 }
+
+
