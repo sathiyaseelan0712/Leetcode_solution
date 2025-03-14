e3acc4bfb996b0e64a3d501847bab2501fad7c31
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-03-14 05:32:01 +0000

    [LeetCode Sync] Runtime - 158 ms (41.18%), Memory - 41.6 MB (77.58%)

--- 0079-word-search/solution.java
+++ 0079-word-search/solution.java
@@ -1,37 +1,34 @@
 class Solution {
-    public boolean exist(char[][] b, String word) {
-        /*Find word's first letter.  Then call method to check it's surroundings */
-        for(int r=0; r<b.length; r++)
-            for(int c=0; c<b[0].length; c++)
-                if(b[r][c]==word.charAt(0) && help(b,word,0,r,c))
-                    return true;
-        
+    public boolean backTrack(char[][] b, String w, boolean[][] v, int i, int j, int idx, int r, int c) {
+        if (idx == w.length())
+            return true;
+        if (i < 0 || j < 0 || i >= r || j >= c || v[i][j] || b[i][j] != w.charAt(idx)) {
+            return false;
+        }
+        v[i][j] = true;
+        if (backTrack(b, w, v, i + 1, j, idx + 1, r, c) ||
+                backTrack(b, w, v, i - 1, j, idx + 1, r, c) ||
+                backTrack(b, w, v, i, j + 1, idx + 1, r, c) ||
+                backTrack(b, w, v, i, j - 1, idx + 1, r, c)) {
+            return true;
+        }
+        v[i][j] = false;
         return false;
     }
-    
-    public boolean help(char[][] b, String word, int start, int r, int c){
-        /* once we get past word.length, we are done. */
-        if(word.length() <= start)
-            return true;
-        
-        /* if off bounds, letter is seen, letter is unequal to word.charAt(start) return false */
-        if(r<0 ||c<0 || r>=b.length || c>=b[0].length || b[r][c]=='0' || b[r][c]!=word.charAt(start))
-            return false;
-        
-        /* set this board position to seen. (Because we can use this postion) */
-        char tmp = b[r][c];
-        b[r][c] = '0';
-        
-        /* recursion on all 4 sides for next letter, if works: return true */
-        if(help(b,word,start+1,r+1,c) ||
-          help(b,word,start+1,r-1,c) ||
-          help(b,word,start+1,r,c+1) ||
-          help(b,word,start+1,r,c-1))
-            return true;
-        
-        //Set back to unseen
-        b[r][c] = tmp;
-        
+
+    public boolean exist(char[][] board, String word) {
+        int r = board.length;
+        int c = board[0].length;
+        boolean[][] visited = new boolean[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    boolean res = backTrack(board, word, visited, i, j, 0, r, c);
+                    if (res)
+                        return true;
+                }
+            }
+        }
         return false;
     }
 }
