04ef04457a827cc73a83f51270d8e13fb2f1e1ea
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-03-15 14:05:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.9 MB (41.86%)

--- 0191-number-of-1-bits/solution.java
+++ 0191-number-of-1-bits/solution.java
@@ -1,15 +1,10 @@
 class Solution {
-    public static int countOne(String s){
-        int len = s.length();
-        if(len == 0) return 0;
-        if(s.charAt(len-1) == '1'){
-            return 1+countOne(s.substring(0,len-1));
-        };
-        return countOne(s.substring(0,len-1));
-    }
     public int hammingWeight(int n) {
-        String str = Integer.toBinaryString(n);
-        return countOne(str);
-        // return count;
+        int cnt = 0;
+        while(n!=0){
+            if((n&1)==1) cnt++;
+            n = n >> 1;
+        }
+        return cnt;
     }
 }
