f9cb74a9676743639edc72e3fbd3ede97ce7a487
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-03-16 09:24:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.2 MB (90.12%)

---
+++ 0319-bulb-switcher/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    // public boolean[] round(boolean[] arr, int n, int x) {
+    //     for (int i = 1; i <= n; i++) {
+    //         if (i % x == 0)
+    //             arr[i - 1] = !arr[i - 1];
+    //     }
+    //     return arr;
+    // }
+
+    // public int checkOn(boolean[] arr, int n) {
+    //     int count = 0;
+    //     for (int i = 0; i < n; i++) {
+    //         if (arr[i] == true) {
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
+
+    // public int bulbSwitch(int n) {
+    //     boolean[] arr = new boolean[n];
+    //     for (int i = 1; i <= n; i++) {
+    //         arr = round(arr, n, i);
+    //     }
+    //     return checkOn(arr, n);
+    // }
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
