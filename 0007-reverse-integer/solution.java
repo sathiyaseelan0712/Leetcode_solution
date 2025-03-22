0e7bf4a74cb0f2f8cc9e957081f069d2af287a6b
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-03-22 17:14:36 +0000

    [LeetCode Sync] Runtime - 1 ms (83.84%), Memory - 41.1 MB (43.80%)

--- 0007-reverse-integer/solution.java
+++ 0007-reverse-integer/solution.java
@@ -1,16 +1,15 @@
 class Solution {
     public int reverse(int x) {
-        int rev= 0;
+        int rev = 0;
         while (x != 0) {
             int rem = x % 10;
             int sum = rev * 10 + rem;
             if ((sum - rem) / 10 != rev) {
-                return 0; 
+                return 0;
             }
-            rev= sum;
+            rev = sum;
             x /= 10;
         }
         return rev;
     }
 }
-
