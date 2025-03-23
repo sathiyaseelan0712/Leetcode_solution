25e047e73bc0224acd9b1b1e9b84407692b65557
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-03-23 15:19:38 +0000

    [LeetCode Sync] Runtime - 1 ms (55.72%), Memory - 40.9 MB (58.51%)

--- 0263-ugly-number/solution.java
+++ 0263-ugly-number/solution.java
@@ -1,5 +1,12 @@
 class Solution {
     public boolean isUgly(int n) {
-        return n<=0 ? false : (n%3==0 ? isUgly(n/3):(n%5==0 ? isUgly(n/5):((n&(n-1))==0)));
+        if(n <= 0) return false;
+        while( n != 1){
+            if(n%2 == 0) n = n/2;
+            else if(n%3 == 0) n= n/3;
+            else if(n%5 == 0) n= n/5;
+            else return false;
+        }
+        return true;
     }
 }
