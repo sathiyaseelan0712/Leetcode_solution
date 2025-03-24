5c44d00363d8dca9bd31ff5b36fbcd562ac2449a
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-03-24 09:08:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.7 MB (23.30%)

--- 3371-harshad-number/solution.java
+++ 3371-harshad-number/solution.java
@@ -1,16 +1,15 @@
 class Solution {
     public int sum(int n){
         int sum = 0;
-        while(n!=0){
-            int rem = n%10;
-            sum+=rem;
+        while( n != 0){
+            sum += (n%10);
             n /= 10;
         }
         return sum;
     }
     public int sumOfTheDigitsOfHarshadNumber(int x) {
-        int val = sum(x);
-        if(x%val == 0) return val;
+        int a = sum(x);
+        if(x%a == 0) return a;
         return -1;
     }
 }
