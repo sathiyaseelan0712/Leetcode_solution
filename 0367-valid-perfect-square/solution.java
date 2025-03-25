5285229a6c60b7d6fdaa86dad35838697fd4df6b
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-03-25 06:30:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.4 MB (67.72%)

--- 0367-valid-perfect-square/solution.java
+++ 0367-valid-perfect-square/solution.java
@@ -1,26 +1,21 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-       if(num<1){
+        long left = 1,right = num;
+        while(left<=right){
+            long mid = left + ((right - left) >> 1);
+            if(mid * mid == num){
+                return true;
+            }
+            if(mid * mid > num){
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
         return false;
-       }
-       if(num==1){
-        return true;
-       }
-       long left=1,right=num;
-       while(left<=right){
-        long mid= left+(right-left)/2;
-        long sum=mid*mid;
-        if(sum<num){
-
-            left=mid+1;
-        }
-        else if(sum>num){
-            right=mid-1;
-        }
-        else{
-            return true;
-        }
-       }
-       return false;
     }
 }
+
+
+
