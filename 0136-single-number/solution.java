5a239f8896b92e49a11a62b28253882b9de10d60
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-03-31 05:38:01 +0000

    [LeetCode Sync] Runtime - 1 ms (99.85%), Memory - 46.2 MB (59.80%)

--- 0136-single-number/solution.java
+++ 0136-single-number/solution.java
@@ -1,7 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = 0;
-        for(int i:nums) ans = ans ^ i;
-        return ans;
+        int res = 0;
+        for(int i : nums){
+            res ^= i;
+        }
+        return res;
     }
 }
