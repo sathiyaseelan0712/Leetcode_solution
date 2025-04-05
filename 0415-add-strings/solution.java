80711c9c31b6878af86e57129ce05e0a81a37698
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-04-05 15:44:52 +0000

    [LeetCode Sync] Runtime - 20 ms (15.67%), Memory - 44.1 MB (17.53%)

--- 0415-add-strings/solution.java
+++ 0415-add-strings/solution.java
@@ -1,9 +1,8 @@
-import java.math.BigInteger; 
+import java.math.*;
 class Solution {
     public String addStrings(String num1, String num2) {
         BigInteger n1 = new BigInteger(num1);
         BigInteger n2 = new BigInteger(num2);
-        return n1.add(n2).toString();
+        return String.valueOf(n1.add(n2));
     }
 }
-
