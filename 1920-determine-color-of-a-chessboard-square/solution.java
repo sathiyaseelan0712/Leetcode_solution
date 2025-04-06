e66098111bab7bbfc5aadd29bba592e5b6c50e5e
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-04-06 06:44:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.3 MB (64.81%)

--- 1920-determine-color-of-a-chessboard-square/solution.java
+++ 1920-determine-color-of-a-chessboard-square/solution.java
@@ -1,10 +1,9 @@
 class Solution {
-    public boolean squareIsWhite(String c) {
-        int sum = 0;
-        for(int i: c.toCharArray()){
-            sum += i;
-        }
-        if(sum%2==0) return false;
+    public boolean squareIsWhite(String co) {
+        int val = 0;
+        val += co.charAt(0) - 'a' + 1;
+        val += co.charAt(1) - '0' ;
+        if((val&1) == 0) return false;
         return true;
     }
 }
