67dbd7489aafbdabda67db4ba0f93fd27d6be977
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-04-06 05:56:22 +0000

    [LeetCode Sync] Runtime - 1 ms (27.85%), Memory - 41.6 MB (79.41%)

--- 3511-find-the-winning-player-in-coin-game/solution.java
+++ 3511-find-the-winning-player-in-coin-game/solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public String winningPlayer(int x, int y) {
-        int player = Math.min(x,y/4);
-        return (player %2 != 1) ? "Bob" : "Alice";
+        int p = Math.min(x,y/4);
+        return (p % 2 == 1) ? "Alice" : "Bob";
     }
 }
