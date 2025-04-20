d2416ab260345af2d5d4f1c2bf66a29b59d7fabf
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-04-20 08:39:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.6 MB (5.25%)

--- 0075-sort-colors/solution.java
+++ 0075-sort-colors/solution.java
@@ -1,14 +1,28 @@
 class Solution {
     public void sortColors(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
-        // Add all elements to the priority queue
-        for (int num : nums) {
-            pq.add(num);
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+        while(mid <= high){
+            switch(nums[mid]){
+                case 0 :
+                    int temp = nums[low];
+                    nums[low] = nums[mid];
+                    nums[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+                case 2:
+                    int temp1 = nums[high];
+                    nums[high] = nums[mid];
+                    nums[mid] = temp1;
+                    high--;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+            }
         }
-
-        for (int i = nums.length-1; i >= 0; i--) {
-            nums[i] = pq.poll();
-        }
+        // return nums;
     }
 }
