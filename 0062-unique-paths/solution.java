4be6e22a38c0e301665e9450aeb2e7d78cff5b57
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-04-20 06:10:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.7 MB (37.22%)

--- 0062-unique-paths/solution.java
+++ 0062-unique-paths/solution.java
@@ -1,15 +1,25 @@
 class Solution {
-    public int solve(int i,int j,Map<String,Integer> dp,int m,int n){
-        if( j >=n || i>=m ) return 0;
-        if(i == m-1 || j== n-1) return 1;
-        String key = i +" "+j;
-        if(dp.containsKey(key)) return dp.get(key);
-        int val = solve(i+1,j,dp,m,n) + solve(i,j+1,dp,m,n);
-        dp.put(key,val);
-        return val;
-    }
+    // private int solve(int i,int j,int n,int m,int[][] arr){
+    //     if(i>=n || j>=m) return 0;
+    //     if(i == n - 1 && j == m-1) return 1;
+    //     if(arr[i][j] != -1) return arr[i][j];
+    //     return arr[i][j] = solve(i+1,j,n,m,arr) + solve(i,j+1,n,m,arr);
+    //     // return arr[i][j];
+    // }
     public int uniquePaths(int m, int n) {
-        Map<String,Integer> dp = new HashMap<>();
-        return solve(0,0,dp,m,n);
+        // int[][] arr = new int[m][n];
+        // for(int i = 0;i<m;i++){
+        //     for(int j = 0;j<n;j++){
+        //         arr[i][j] = -1;
+        //     }
+        // }
+        // return solve(0,0,m,n,arr);
+        int N = m+n-2;
+        int r = m-1;
+        double res =1;
+        for(int i = 1;i<=r;i++){
+            res = res*(N-r+i)/i;
+        }
+        return (int)res;
     }
 }
