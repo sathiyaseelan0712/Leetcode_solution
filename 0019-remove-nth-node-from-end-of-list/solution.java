52cfbdcd5ef36133eb87e4df3486bcb5e0a33287
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-04-23 06:00:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.6 MB (87.86%)

--- 0019-remove-nth-node-from-end-of-list/solution.java
+++ 0019-remove-nth-node-from-end-of-list/solution.java
@@ -1,31 +1,31 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     private int length(ListNode head){
-        ListNode temp = new ListNode();
-        temp = head;
-        int count =0;
-        while(temp!=null){
-            count++;
-            temp=temp.next;
+        int cnt = 0;
+        ListNode cur = new ListNode();
+        cur = head;
+        while(cur != null){
+            cnt++;
+            cur = cur.next;
         }
-        return count;
+        return cnt;
     }
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode temp = head;
+        ListNode temp = new ListNode();
         int len = length(head);
+        temp = head;
         int pos = len - n;
-        if(len == 1){
-            return null;
-        }
+        if(len == 1) return null;
+
         int cur = 1;
         while(cur < pos){
             temp = temp.next;
@@ -33,6 +33,7 @@ class Solution {
         }
         if(len == n) head = head.next;
         else temp.next = temp.next.next;
-        return head;     
+
+        return head;
     }
 }
