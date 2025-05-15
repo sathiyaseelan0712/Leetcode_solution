dfeeeb5a131d9217b09e6dd9d3a183afd78b3719
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-05-15 16:42:27 +0000

    [LeetCode Sync] Runtime - 3 ms (75.45%), Memory - 55.7 MB (26.87%)

---
+++ 1372-check-if-it-is-a-good-array/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+     public int gcd(int a, int b){
+        if(b==0)
+            return a;
+        return gcd(b,a%b);
+    }
+    public boolean isGoodArray(int[] nums) {
+        int gcd = nums[0];
+        for(int i = 1; i<nums.length; i++){
+            gcd = gcd(gcd, nums[i]);
+            if(gcd == 1)
+                return true;
+        }
+        return gcd==1;
+    }
+}
