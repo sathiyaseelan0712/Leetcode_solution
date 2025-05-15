e97fbc570f6aa75fabd9d3d94bff9b4b34fc786f
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-05-15 16:41:36 +0000

    [LeetCode Sync] Runtime - 230 ms (77.92%), Memory - 44.7 MB (68.83%)

---
+++ 1024-triples-with-bitwise-and-equal-to-zero/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countTriplets(int[] nums) {
+         int[] count = new int[1 << 16];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                count[nums[i] & nums[j]]++;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < 1 << 16; j++) {
+                if ((nums[i] & j) == 0) {
+                    ans += count[j];
+                }
+            }
+        }
+        return ans;
+    }
+}
