f8c91c35afdac9cc587561bd2c87221481efa1b6
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-06-18 04:45:44 +0000

    [LeetCode Sync] Runtime - 28 ms (80.40%), Memory - 51.4 MB (83.24%)

--- 0015-3sum/solution.java
+++ 0015-3sum/solution.java
@@ -1,35 +1,54 @@
 class Solution {
-  public List<List<Integer>> threeSum(int[] nums) {
-    if (nums.length < 3)
-      return new ArrayList<>();
-
-    List<List<Integer>> ans = new ArrayList<>();
-
-    Arrays.sort(nums);
-
-    for (int i = 0; i + 2 < nums.length; ++i) {
-      if (i > 0 && nums[i] == nums[i - 1])
-        continue;
-      // Choose nums[i] as the first number in the triplet, then search the
-      // remaining numbers in [i + 1, n - 1].
-      int l = i + 1;
-      int r = nums.length - 1;
-      while (l < r) {
-        final int sum = nums[i] + nums[l] + nums[r];
-        if (sum == 0) {
-          ans.add(Arrays.asList(nums[i], nums[l++], nums[r--]));
-          while (l < r && nums[l] == nums[l - 1])
-            ++l;
-          while (l < r && nums[r] == nums[r + 1])
-            --r;
-        } else if (sum < 0) {
-          ++l;
-        } else {
-          --r;
+    private static void backTrack(int[] nums,int start,List<Integer> temp,List<List<Integer>> ans){
+        if(temp.size() == 3){
+            int sum = temp.get(0) + temp.get(1) + temp.get(2);
+            if(sum == 0){
+                if(!ans.contains(temp)){
+                    ans.add(new ArrayList<>(temp));
+                }
+            }
+            return;
         }
-      }
-    }
 
-    return ans;
-  }
+        for(int i = start;i<nums.length;i++){
+            temp.add(nums[i]);
+            backTrack(nums,i+1,temp,ans);
+            temp.remove(temp.size()-1);
+        }
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        
+        Arrays.sort(nums);
+
+        for(int i = 0;i+2<n;i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+
+            int left = i+1;
+            int right = n-1;
+
+            while(left  < right){
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i],nums[left++],nums[right--]));
+                    while(left < right && nums[left] == nums[left-1]){
+                        left++;
+                    }
+                    while(left<right && nums[right] == nums[right+1]){
+                        right--;
+                    }
+                }else if(sum < 0){
+                    left++;
+                }else{
+                    right--;
+                }
+            }
+        }
+        // backTrack(nums,0,new ArrayList<>(), ans);
+        return ans;
+    }
 }
