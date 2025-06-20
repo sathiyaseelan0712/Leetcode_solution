1f67f3a3001dca3239d06f55c60c4f8490a75de1
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-06-20 08:21:19 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 44.7 MB (41.53%)

--- 0002-add-two-numbers/solution.java
+++ 0002-add-two-numbers/solution.java
@@ -11,23 +11,27 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0);
-        ListNode temp = head;
+        ListNode cur = head;
         int carry = 0;
-        while(l1 != null || l2 != null || carry ==1){
+
+        while(l1 != null || l2 != null || carry != 0){
             int sum = 0;
-            if(l1 != null) {
-                sum+=l1.val;
-                l1 = l1.next;
-            } 
-            if(l2 != null ){
-                sum+=l2.val;
+
+            if(l1 != null){
+                sum += l1.val;
+                l1= l1.next;
+            }
+            if(l2 != null){
+                sum += l2.val;
                 l2 = l2.next;
             }
-            sum+=carry;
-            carry = sum / 10;
-            ListNode temp1 = new ListNode(sum%10);
-            temp.next = temp1;
-            temp = temp1;
+
+            sum += carry;
+
+            ListNode temp = new ListNode(sum%10);
+            carry = sum  /10;
+            cur.next = temp;
+            cur = temp;
         }
         return head.next;
     }
