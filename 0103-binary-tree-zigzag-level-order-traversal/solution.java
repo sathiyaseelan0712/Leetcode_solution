aea0098f70a43f1cdaa6e1db92afba6c422916aa
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-06-22 11:47:23 +0000

    [LeetCode Sync] Runtime - 1 ms (65.93%), Memory - 42.2 MB (58.43%)

--- 0103-binary-tree-zigzag-level-order-traversal/solution.java
+++ 0103-binary-tree-zigzag-level-order-traversal/solution.java
@@ -1,41 +1,41 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
+        Queue<TreeNode> qu = new LinkedList<>();
+
         if(root == null) return res;
-        q.add(root);
-        int flag = 1;
-        while (!q.isEmpty()) {
+        qu.offer(root);
+        boolean isLeftTORight = false;
+
+        while(!qu.isEmpty()){
             List<Integer> ans = new ArrayList<>();
-            for (int count = q.size(); count != 0; count--) {
-                TreeNode curr = q.remove();
-                ans.add(curr.val);
-                if (curr.left != null)
-                    q.add(curr.left);
-                if (curr.right != null)
-                    q.add(curr.right);
+            int size = qu.size();
+            while(size != 0){
+                TreeNode cur = qu.poll();
+                ans.add(cur.val);
+                if(cur.left != null) qu.offer(cur.left);
+                if(cur.right != null) qu.offer(cur.right);
+                size--;
             }
-            if (flag == 0) {
+            if(isLeftTORight){
                 Collections.reverse(ans);
-                flag = 1;
-            } else{
-                flag = 0;
             }
+            isLeftTORight = !isLeftTORight;
             res.add(ans);
         }
         return res;
