0a9fec19d2ceb7d0abf11d05f6c4664fbadab4d9
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-06-22 06:31:41 +0000

    [LeetCode Sync] Runtime - 1 ms (99.70%), Memory - 63.4 MB (48.96%)

--- 0111-minimum-depth-of-binary-tree/solution.java
+++ 0111-minimum-depth-of-binary-tree/solution.java
@@ -15,10 +15,21 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-       if(root == null) return 0;
-       if(root.left == null && root.right == null) return 1;
-       if(root.left == null) return minDepth(root.right) + 1;
-       if(root.right == null) return minDepth(root.left) + 1;
-       return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        if(root == null ) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 1;
+        while(!queue.isEmpty()){
+            int size =queue.size();
+            while(size != 0){
+                TreeNode cur = queue.poll();
+                if(cur.left == null && cur.right == null) return depth;
+                if(cur.left != null) queue.offer(cur.left);
+                if(cur.right != null) queue.offer(cur.right);
+                size--;
+            }
+            depth++;
+        }
+        return depth;
     }
 }
