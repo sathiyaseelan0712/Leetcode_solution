efbfd80ab98fb388f8452fe21aac938f6b26ecd6
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-06-27 06:30:52 +0000

    [LeetCode Sync] Runtime - 1 ms (99.85%), Memory - 44 MB (89.63%)

--- 0120-triangle/solution.java
+++ 0120-triangle/solution.java
@@ -1,20 +1,26 @@
 class Solution {
-   public int minimumTotal(List<List<Integer>> triangle) {
-        int m = triangle.size();
-        int n = triangle.get(m - 1).size();
-        int[][] dp= new int[m][n];
-        
-        for(int i=0; i< n; i++){
-            dp[m-1][i]= triangle.get(m-1).get(i);
+    int m;
+    int n;
+    int[][] dp;
+    List<List<Integer>> arr;
+    public int solve(int i, int  j){
+        // if(i < 0 || j < 0 || i >= m || j >= n) return Integer.MAX_VALUE;
+        // System.out.println(arr.get(i).get(j) + " " + i + " " +j);
+        if(i == m-1) return arr.get(i).get(j);
+        if(dp[i][j] != -1) return dp[i][j];
+        int val = arr.get(i).get(j) + Math.min(solve(i+1,j), solve(i+1,j+1));
+        dp[i][j] = val;
+        return val;
+    }
+    public int minimumTotal(List<List<Integer>> triangle) {
+        arr = triangle;
+        m = triangle.size();
+        n = triangle.get(m-1).size();
+        dp = new int[m][n];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
         }
-
-        for(int i= m-2; i>= 0; i--){
-            for(int j= i; j>= 0; j--){
-                int up= triangle.get(i).get(j) + dp[i+1][j];
-                int upRight= triangle.get(i).get(j) + dp[i+1][j+1];
-                dp[i][j]= Math.min(up, upRight);
-            }
-        }
-        return dp[0][0];  
+        return solve(0,0);
+        // return dp[0][0];
     }
 }
