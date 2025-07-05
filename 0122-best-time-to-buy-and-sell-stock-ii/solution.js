b933256098591a8f27fa6f9210c99bada739b679
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-07-05 16:13:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 54.5 MB (32.06%)

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let sum = 0;
+    for(let i = 0;i<prices.length;i++){
+        if(prices[i] < prices[i+1]){
+            sum += prices[i+1] - prices[i];
+        }
+    }
+    return sum;
+};
