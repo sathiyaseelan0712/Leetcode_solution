aabafa8562103a13a11ee077b5218ae9b573b6a7
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-07-08 18:10:37 +0000

    [LeetCode Sync] Runtime - 27 ms (86.88%), Memory - 51.8 MB (41.24%)

--- 0015-3sum/solution.java
+++ 0015-3sum/solution.java
@@ -1,5 +1,5 @@
 class Solution {
-    private static void backTrack(int[] nums,int start,List<Integer> temp,List<List<Integer>> ans){
+    public void bt(List<List<Integer>> ans, int[] nums,int start,List<Integer> temp){
         if(temp.size() == 3){
             int sum = temp.get(0) + temp.get(1) + temp.get(2);
             if(sum == 0){
@@ -9,28 +9,24 @@ class Solution {
             }
             return;
         }
-
         for(int i = start;i<nums.length;i++){
             temp.add(nums[i]);
-            backTrack(nums,i+1,temp,ans);
+            bt(ans,nums,i+1, temp);
             temp.remove(temp.size()-1);
         }
     }
     public List<List<Integer>> threeSum(int[] nums) {
-        
         List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
-        
         Arrays.sort(nums);
 
         for(int i = 0;i+2<n;i++){
             if(i>0 && nums[i] == nums[i-1]) continue;
 
             int left = i+1;
-            int right = n-1;
+            int right = n - 1;
 
-            while(left  < right){
-
+            while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
 
                 if(sum == 0){
@@ -38,7 +34,7 @@ class Solution {
                     while(left < right && nums[left] == nums[left-1]){
                         left++;
                     }
-                    while(left<right && nums[right] == nums[right+1]){
+                    while(left < right && nums[right] == nums[right+1]){
                         right--;
                     }
                 }else if(sum < 0){
@@ -48,7 +44,7 @@ class Solution {
                 }
             }
         }
-        // backTrack(nums,0,new ArrayList<>(), ans);
+        // bt(ans, nums, 0,new ArrayList<>());
         return ans;
     }
 }
