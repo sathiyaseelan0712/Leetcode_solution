17e06a75205ba217a1fe56deee4a122482d68208
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-07-11 18:12:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.5 MB (64.50%)

--- 0061-rotate-list/solution.java
+++ 0061-rotate-list/solution.java
@@ -1,20 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-  public ListNode rotateRight(ListNode head, int k) {
-    if (head == null || head.next == null || k == 0)
-      return head;
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) return head;
+       int len = 1;
+       ListNode tail = head;
+       while(tail.next != null){
+            len++;
+            tail = tail.next;
+       }
+       k = k%len;
+       tail.next = head;
+       ListNode newTail = head;
+       int newLen = len - k;
+       for(int i = 1;i<newLen;i++){
+            newTail = newTail.next;
+       }
+       ListNode newHead = newTail.next;
+       newTail.next = null;
 
-    int length = 1;
-    ListNode tail = head;
-    for (; tail.next != null; tail = tail.next)
-      ++length;
-    tail.next = head; // Circle the list.
-
-    final int t = length - k % length;
-    for (int i = 0; i < t; ++i)
-      tail = tail.next;
-    ListNode newHead = tail.next;
-    tail.next = null;
-
-    return newHead;
-  }
+       return newHead;
+    }
 }
