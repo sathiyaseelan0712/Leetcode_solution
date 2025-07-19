3505615f13f667104668fafa6b8fd2a1233d594c
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-07-19 09:20:17 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 42.6 MB (38.30%)

--- 1036-rotting-oranges/solution.java
+++ 1036-rotting-oranges/solution.java
@@ -2,12 +2,12 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        Queue<int[]> qu = new LinkedList<>();
+        Queue<int[]> queue = new LinkedList<>();
         int cntFresh = 0;
         for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
+            for(int j =0;j<n;j++){
                 if(grid[i][j] == 2){
-                    qu.offer(new int[]{i,j});
+                    queue.offer(new int[]{i,j});
                 }
                 if(grid[i][j] == 1){
                     cntFresh++;
@@ -15,29 +15,37 @@ class Solution {
             }
         }
         if(cntFresh == 0) return 0;
-        if(qu.isEmpty()) return -1;
+        if(queue.isEmpty()) return -1;
         
-        int min = -1;
-        int[][] direction = { {1,0},{-1,0},{0,1},{0,-1}};
-        while(!qu.isEmpty()){
-            int size = qu.size();
-            for(int i =0;i<size;i++){
-                int[] cur = qu.poll();
+        int[][] dir = {
+            {0,1},
+            {1,0},
+            {0,-1},
+            {-1,0}
+        };
+
+        int ans = -1;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            while(size != 0){
+                int[] cur = queue.poll();
                 int x = cur[0];
                 int y = cur[1];
-                for(int[] dir : direction){
-                    int a = x+dir[0];
-                    int b = y+dir[1];
-                    if( a >=0 && a <m && b>=0 && b < n && grid[a][b] == 1){
+                for(int i = 0;i<4;i++){
+                    int x1 = x + dir[i][0];
+                    int y1 = y + dir[i][1];
+                    if(x1 >= 0 && x1 < m && y1 >= 0 && y1 < n && grid[x1][y1] == 1){
                         cntFresh--;
-                        grid[a][b] = 2;
-                        qu.offer(new int[] {a,b});
+                        grid[x1][y1] = 2;
+                        queue.add(new int[]{x1,y1});
                     }
-                }
+                }  
+                size--;              
             }
-            min++;
+            ans++;
+ 
         }
-        if(cntFresh == 0) return min;
+        if(cntFresh == 0) return ans;
         return -1;
     }
 }
