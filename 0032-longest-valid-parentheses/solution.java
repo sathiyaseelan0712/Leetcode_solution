711191dcbcabd59254339a233e45952fc799f602
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-07-21 10:25:00 +0000

    [LeetCode Sync] Runtime - 3 ms (78.55%), Memory - 42.8 MB (78.02%)

--- 0032-longest-valid-parentheses/solution.java
+++ 0032-longest-valid-parentheses/solution.java
@@ -1,21 +1,21 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(-1);
-        int maxLen = 0;
+        Stack<Integer> st = new Stack();
+        st.push(-1);
+        int max_len = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stack.push(i);
-            } else {
-                stack.pop();
-                if (stack.isEmpty()) {
-                    stack.push(i); // reset base
-                } else {
-                    maxLen = Math.max(maxLen, i - stack.peek());
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i) == '('){
+                st.push(i);
+            }else{
+                st.pop();
+                if(st.isEmpty()){
+                    st.push(i);
+                }else{
+                    max_len = Math.max(max_len, i - st.peek());
                 }
             }
         }
-        return maxLen;
+        return max_len;
     }
 }
