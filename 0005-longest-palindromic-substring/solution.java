257c81928fb5d84c32ccb7226e9aed6af9d09dee
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2025-08-11 15:23:37 +0000

    [LeetCode Sync] Runtime - 13 ms (95.88%), Memory - 42.1 MB (87.87%)

--- 0005-longest-palindromic-substring/solution.java
+++ 0005-longest-palindromic-substring/solution.java
@@ -1,25 +1,23 @@
 class Solution {
     private int start, maxLen;
     public String longestPalindrome(String s) {
-        int len = s.length();
-        if(len < 2){
-            return s;
-        }
-        for(int i = 0;i<len-1;i++){
+        int n = s.length();
+        if(n < 2) return s;
+        for(int i = 0;i<n-1;i++){
             solve(s,i,i);
             solve(s,i,i+1);
         }
-        return s.substring(start, start + maxLen);
+        // System.out.println(start + " " + maxLen);
+        return s.substring(start, start+maxLen);
     }
-
-    private void solve(String s, int j,int k){
+    private void solve(String s, int j, int k){
         while(j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)){
             j--;
             k++;
         }
-        if(maxLen < k-j-1){
+        if(maxLen  < k -j-1){
             start = j+1;
-            maxLen = k-j-1;
+            maxLen  = k-j-1;
         }
     }
 }
